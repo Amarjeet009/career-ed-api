c9cca75a2a3cec45178eb37197ec9917100d7ac9
Amarjeet Rao <amarjeetrao@outlook.com>
2025-08-24 02:33:58 +0530

    Boiler Template update

---
+++ src/main/kotlin/com/career/ed/CareerEdApiApplication.kt
@@ -0,0 +1,11 @@
+package com.career.ed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CareerEdApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CareerEdApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/career/ed/CareerEdApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.career.ed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CareerEdApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
